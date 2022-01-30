468c06fb7dd0f5c81acae94c4c0453c9594db141
Jonathan Kahonen <jonathan.kahonen@auntiesolutionsfi.onmicrosoft.com>
2022-01-30 11:36:56 +0200

    I programmed the registration functionality. No users with same name can be created.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 # The database URL is fetched from environmental variable in .env file.
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
@@ -14,21 +16,138 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT content FROM messages")
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages) 
+    sql = "SELECT id, topic, created_at FROM polls ORDER BY id DESC"
+    result = db.session.execute(sql)
+    polls = result.fetchall()
+    return render_template("index.html", polls=polls)
+    
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if not user:
+        # TODO: invalid username
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            # TODO: correct username and password
+        else:
+            # TODO: invalid password
+    
+    session["username"] = username
+    return redirect("/")
 
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")    
+
+@app.route("/init_registration/<int:id>")
+def init_registration(id):
+    if id == 0:
+        return  render_template("register.html", error="")
+    else:
+        return  render_template("register.html", error="User with that name already exists")
+
+@app.route("/register", methods=["POST"])
+def register():
+    registration_success = False
+    username_to_add = request.form["username"]
+    sql_find_existing_user = "SELECT DISTINCT username FROM users WHERE username = :username_to_add";
+    result = db.session.execute(sql_find_existing_user, {"username_to_add":username_to_add})
+    existing_user = result.fetchone()
+    already_exists = existing_user != None and existing_user[0] == username_to_add
+
+    if not already_exists:
+        hash_value = generate_password_hash(request.form["password"])
+        user_role = "USER"
+        sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)"
+        db.session.execute(sql, {"username":username_to_add, "password":hash_value, "role":user_role})
+        db.session.commit()
+        registration_success = True
+    
+    if registration_success:
+        return redirect("/registration_complete")
+    else:
+        return redirect("/init_registration/1")
+    
+@app.route("/registration_complete")
+def registration_complete():
+    return render_template("registration_complete.html")
+
+def new():
+    return render_template("new.html")
+    
 @app.route("/new")
 def new():
     return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
+    
+@app.route("/create", methods=["POST"])
+def create():
+    topic = request.form["topic"]
+    sql = "INSERT INTO polls (topic, created_at) VALUES (:topic, NOW()) RETURNING id"
+    result = db.session.execute(sql, {"topic":topic})
+    poll_id = result.fetchone()[0]
+    choices = request.form.getlist("choice")
+    for choice in choices:
+        if choice != "":
+            sql = "INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)"
+            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
     db.session.commit()
     return redirect("/")
+    
+@app.route("/poll/<int:id>")
+def poll(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT id, choice FROM choices WHERE poll_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    choices = result.fetchall()
+    return render_template("poll.html", id=id, topic=topic, choices=choices)
+    
+@app.route("/answer", methods=["POST"])
+def answer():
+    poll_id = request.form["id"]
+    if "answer" in request.form:
+        choice_id = request.form["answer"]
+        sql = "INSERT INTO answers (choice_id, sent_at) VALUES (:choice_id, NOW())"
+        db.session.execute(sql, {"choice_id":choice_id})
+        db.session.commit()
+    return redirect("/result/" + str(poll_id))
+    
+@app.route("/result/<int:id>")
+def result(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
+          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
+    result = db.session.execute(sql, {"poll_id":id})
+    choices = result.fetchall()
+    return render_template("result.html", topic=topic, choices=choices)
+
+#@app.route("/")
+#def index():
+#    result = db.session.execute("SELECT content FROM messages")
+#    messages = result.fetchall()
+#    return render_template("index.html", count=len(messages), messages=messages) 
+
+#@app.route("/new")
+#def new():
+#    return render_template("new.html")
+
+#@app.route("/send", methods=["POST"])
+#def send():
+#    content = request.form["content"]
+#    sql = "INSERT INTO messages (content) VALUES (:content)"
+#    db.session.execute(sql, {"content":content})
+#    db.session.commit()
+#    return redirect("/")
 
 #@app.route("/")
 #def index():
